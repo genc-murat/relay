5d5b900e73fc66b8c9a6f8f2a1ba444f6fbcd170
Murat Genc <muttafi@gmail.com>
2025-10-13 22:52:34 +0300

    refactor: Remove NotificationPublishingStrategy from NotificationPublisherOptions and create a new file for predefined notification publishing strategies

--- src/Relay.Core/Publishing/Options/NotificationPublisherOptions.cs
+++ src/Relay.Core/Publishing/Options/NotificationPublisherOptions.cs
@@ -1,6 +1,5 @@
 using System;
 using Relay.Core.Publishing.Interfaces;
-using Relay.Core.Publishing.Strategies;
 
 namespace Relay.Core.Publishing.Options
 {
@@ -34,30 +33,5 @@ namespace Relay.Core.Publishing.Options
         /// </summary>
         public Microsoft.Extensions.DependencyInjection.ServiceLifetime Lifetime { get; set; }
             = Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton;
-    }
-
-    /// <summary>
-    /// Predefined notification publishing strategies.
-    /// </summary>
-    public static class NotificationPublishingStrategy
-    {
-        /// <summary>
-        /// Sequential publishing - handlers execute one at a time in order.
-        /// Stops on first exception. Safest but slowest.
-        /// </summary>
-        public static Type Sequential => typeof(SequentialNotificationPublisher);
-
-        /// <summary>
-        /// Parallel publishing - handlers execute concurrently using Task.WhenAll.
-        /// Stops on first exception. Fast but requires thread-safe handlers.
-        /// </summary>
-        public static Type Parallel => typeof(ParallelNotificationPublisher);
-
-        /// <summary>
-        /// Parallel publishing with exception tolerance - handlers execute concurrently.
-        /// Continues execution even if handlers fail. All exceptions are collected.
-        /// Ensures all handlers get a chance to execute.
-        /// </summary>
-        public static Type ParallelWhenAll => typeof(ParallelWhenAllNotificationPublisher);
     }
 }

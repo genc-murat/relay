b7f3cc54371d9db185b4db250e9d9fff2de69b16
Murat Genc <muttafi@gmail.com>
2025-10-26 15:15:57 +0300

    test: Enhance GetHandlerName tests to cover various scenarios including valid, whitespace, empty, and missing attributes

--- tests/Relay.SourceGenerator.Tests/GetHandlerNameTests.cs
+++ tests/Relay.SourceGenerator.Tests/GetHandlerNameTests.cs
@@ -6,10 +6,70 @@ namespace Relay.SourceGenerator.Tests;
 public class GetHandlerNameTests
 {
     [Fact]
+    public void GetHandlerName_WithValidName_Should_ReturnThatName()
+    {
+        // Arrange
+        var source = @"
+using System.Threading;
+using System.Threading.Tasks;
+using Relay.Core;
+
+namespace TestProject
+{
+    public class TestRequest : IRequest<string> { }
+    
+    public class TestHandler
+    {
+        [Handle(Name = ""CustomName"")]
+        public Task<string> Handle(TestRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(""test"");
+        }
+    }
+}";
+
+        var compilation = CreateTestCompilation(source);
+        var syntaxTree = compilation.SyntaxTrees.First(st => st.ToString().Contains("TestProject"));
+        var root = syntaxTree.GetRoot();
+        
+        var methodDeclaration = root.DescendantNodes()
+            .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.MethodDeclarationSyntax>()
+            .First(m => m.Identifier.ValueText == "Handle");
+
+        var semanticModel = compilation.GetSemanticModel(syntaxTree);
+        var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration) as IMethodSymbol;
+        
+        var context = new RelayCompilationContext(compilation, default);
+        var generator = new HandlerRegistryGenerator(context);
+
+        // Get the attribute data from the method
+        var attributeData = methodSymbol?.GetAttributes()
+            .FirstOrDefault(attr => attr.AttributeClass?.Name == "HandleAttribute");
+
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = methodSymbol,
+            Attributes = new List<RelayAttributeInfo>
+            {
+                new RelayAttributeInfo { Type = RelayAttributeType.Handle, AttributeData = attributeData }
+            }
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("CustomName", result);
+    }
+
+    [Fact]
     public void GetHandlerName_WhitespaceName_Should_ReturnDefault()
     {
-        // Arrange: Create a handler with [Handle(Name = "   ")] where Name is whitespace
-        // This tests the branch where !string.IsNullOrWhiteSpace(name) is false
+        // Arrange
         var source = @"
 using System.Threading;
 using System.Threading.Tasks;
@@ -33,35 +93,44 @@ namespace TestProject
         var syntaxTree = compilation.SyntaxTrees.First(st => st.ToString().Contains("TestProject"));
         var root = syntaxTree.GetRoot();
         
-        // Get the method declaration
         var methodDeclaration = root.DescendantNodes()
             .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.MethodDeclarationSyntax>()
             .First(m => m.Identifier.ValueText == "Handle");
 
-        // Create a RelayCompilationContext and HandlerDiscoveryEngine
         var semanticModel = compilation.GetSemanticModel(syntaxTree);
         var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration) as IMethodSymbol;
         
         var context = new RelayCompilationContext(compilation, default);
-        var discoveryEngine = new HandlerDiscoveryEngine(context);
+        var generator = new HandlerRegistryGenerator(context);
 
-        // Create a diagnostic reporter
-        var diagnosticReporter = new TestDiagnosticReporter();
+        // Get the attribute data from the method
+        var attributeData = methodSymbol?.GetAttributes()
+            .FirstOrDefault(attr => attr.AttributeClass?.Name == "HandleAttribute");
 
-        // Manually create a HandlerInfo for testing (since we can't easily access private methods)
-        // We'll test the GetHandlerName method indirectly by ensuring the logic works correctly
-        // when the Name property is whitespace
-        
-        // Act & Assert: The method should fall back to "default" when Name is whitespace
-        // We can't directly call GetHandlerName since it's private, but we can verify the behavior
-        // by testing that handlers with whitespace names are treated as having "default" names
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = methodSymbol,
+            Attributes = new List<RelayAttributeInfo>
+            {
+                new RelayAttributeInfo { Type = RelayAttributeType.Handle, AttributeData = attributeData }
+            }
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("default", result);
     }
 
     [Fact]
     public void GetHandlerName_EmptyName_Should_ReturnDefault()
     {
-        // Arrange: Create a handler with [Handle(Name = "")] where Name is empty
-        // This tests the branch where !string.IsNullOrWhiteSpace(name) is false
+        // Arrange
         var source = @"
 using System.Threading;
 using System.Threading.Tasks;
@@ -85,30 +154,183 @@ namespace TestProject
         var syntaxTree = compilation.SyntaxTrees.First(st => st.ToString().Contains("TestProject"));
         var root = syntaxTree.GetRoot();
         
-        // Get the method declaration
         var methodDeclaration = root.DescendantNodes()
             .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.MethodDeclarationSyntax>()
             .First(m => m.Identifier.ValueText == "Handle");
 
-        // Create a RelayCompilationContext and HandlerDiscoveryEngine
         var semanticModel = compilation.GetSemanticModel(syntaxTree);
         var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration) as IMethodSymbol;
         
         var context = new RelayCompilationContext(compilation, default);
-        var discoveryEngine = new HandlerDiscoveryEngine(context);
+        var generator = new HandlerRegistryGenerator(context);
 
-        // Create a diagnostic reporter
-        var diagnosticReporter = new TestDiagnosticReporter();
+        // Get the attribute data from the method
+        var attributeData = methodSymbol?.GetAttributes()
+            .FirstOrDefault(attr => attr.AttributeClass?.Name == "HandleAttribute");
 
-        // Act & Assert: Similar to above, the method should fall back to "default" when Name is empty
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = methodSymbol,
+            Attributes = new List<RelayAttributeInfo>
+            {
+                new RelayAttributeInfo { Type = RelayAttributeType.Handle, AttributeData = attributeData }
+            }
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("default", result);
     }
 
     [Fact]
-    public void GetHandlerName_NullNameProperty_Should_ReturnDefault()
+    public void GetHandlerName_WithoutNameProperty_Should_ReturnDefault()
     {
-        // Arrange: Test the scenario where the Name property exists but has a null value
-        // This is harder to create in C# syntax, but we can document the scenario
-        // The branch "nameArg.Value.Value is string name" would be false for null values
+        // Arrange
+        var source = @"
+using System.Threading;
+using System.Threading.Tasks;
+using Relay.Core;
+
+namespace TestProject
+{
+    public class TestRequest : IRequest<string> { }
+    
+    public class TestHandler
+    {
+        [Handle]  // No Name property specified
+        public Task<string> Handle(TestRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(""test"");
+        }
+    }
+}";
+
+        var compilation = CreateTestCompilation(source);
+        var syntaxTree = compilation.SyntaxTrees.First(st => st.ToString().Contains("TestProject"));
+        var root = syntaxTree.GetRoot();
+        
+        var methodDeclaration = root.DescendantNodes()
+            .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.MethodDeclarationSyntax>()
+            .First(m => m.Identifier.ValueText == "Handle");
+
+        var semanticModel = compilation.GetSemanticModel(syntaxTree);
+        var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration) as IMethodSymbol;
+        
+        var context = new RelayCompilationContext(compilation, default);
+        var generator = new HandlerRegistryGenerator(context);
+
+        // Get the attribute data from the method
+        var attributeData = methodSymbol?.GetAttributes()
+            .FirstOrDefault(attr => attr.AttributeClass?.Name == "HandleAttribute");
+
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = methodSymbol,
+            Attributes = new List<RelayAttributeInfo>
+            {
+                new RelayAttributeInfo { Type = RelayAttributeType.Handle, AttributeData = attributeData }
+            }
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("default", result);
+    }
+
+    [Fact]
+    public void GetHandlerName_WithoutHandleAttribute_Should_ReturnDefault()
+    {
+        // Arrange
+        var source = @"
+using System.Threading;
+using System.Threading.Tasks;
+using Relay.Core;
+
+namespace TestProject
+{
+    public class TestRequest : IRequest<string> { }
+    
+    public class TestHandler
+    {
+        // No Handle attribute at all
+        public Task<string> Handle(TestRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(""test"");
+        }
+    }
+}";
+
+        var compilation = CreateTestCompilation(source);
+        var syntaxTree = compilation.SyntaxTrees.First(st => st.ToString().Contains("TestProject"));
+        var root = syntaxTree.GetRoot();
+        
+        var methodDeclaration = root.DescendantNodes()
+            .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.MethodDeclarationSyntax>()
+            .First(m => m.Identifier.ValueText == "Handle");
+
+        var semanticModel = compilation.GetSemanticModel(syntaxTree);
+        var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration) as IMethodSymbol;
+        
+        var context = new RelayCompilationContext(compilation, default);
+        var generator = new HandlerRegistryGenerator(context);
+
+        // No Handle attribute at all
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = methodSymbol,
+            Attributes = new List<RelayAttributeInfo>() // Empty list, no Handle attribute
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("default", result);
+    }
+
+    [Fact]
+    public void GetHandlerName_WithNullAttributeData_Should_ReturnDefault()
+    {
+        // Arrange
+        var compilation = CreateTestCompilation("");
+        var context = new RelayCompilationContext(compilation, default);
+        var generator = new HandlerRegistryGenerator(context);
+
+        // Create a handler with handle attribute but null AttributeData
+        var handlerInfo = new HandlerInfo
+        {
+            MethodSymbol = null,
+            Attributes = new List<RelayAttributeInfo>
+            {
+                new RelayAttributeInfo { Type = RelayAttributeType.Handle, AttributeData = null }
+            }
+        };
+
+        // Use reflection to access the private GetHandlerName method
+        var getHandlerNameMethod = typeof(HandlerRegistryGenerator).GetMethod("GetHandlerName",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var result = getHandlerNameMethod?.Invoke(generator, new object[] { handlerInfo });
+
+        // Assert
+        Assert.Equal("default", result);
     }
 
     private static Compilation CreateTestCompilation(string source)

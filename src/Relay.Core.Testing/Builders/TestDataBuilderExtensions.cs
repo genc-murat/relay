7683781313e0bc9669a8dedb6daa4728fe505ef9
Murat Genc <muttafi@gmail.com>
2025-11-08 10:50:29 +0300

    refactor: Remove unused TestDataBuilderExtensions class and associated methods

---
+++ src/Relay.Core.Testing/Builders/TestDataBuilderExtensions.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq.Expressions;
+
+namespace Relay.Core.Testing;
+
+/// <summary>
+/// Extension methods for test data builders.
+/// </summary>
+public static class TestDataBuilderExtensions
+{
+    /// <summary>
+    /// Sets a property value using an expression.
+    /// </summary>
+    /// <typeparam name="T">The type of object being built.</typeparam>
+    /// <typeparam name="TProperty">The type of the property.</typeparam>
+    /// <param name="builder">The builder instance.</param>
+    /// <param name="propertyExpression">Expression identifying the property.</param>
+    /// <param name="value">The value to set.</param>
+    /// <returns>The current builder instance for method chaining.</returns>
+    public static TestDataBuilder<T> WithProperty<T, TProperty>(
+        this TestDataBuilder<T> builder,
+        Expression<Func<T, TProperty>> propertyExpression,
+        TProperty value) where T : class
+    {
+        var memberExpression = propertyExpression.Body as MemberExpression;
+        if (memberExpression == null)
+        {
+            throw new ArgumentException("Expression must be a member expression", nameof(propertyExpression));
+        }
+
+        var propertyInfo = typeof(T).GetProperty(memberExpression.Member.Name);
+        if (propertyInfo == null)
+        {
+            throw new ArgumentException($"Property '{memberExpression.Member.Name}' not found on type '{typeof(T).Name}'");
+        }
+
+        propertyInfo.SetValue(builder.Instance, value);
+        return builder;
+    }
+}
